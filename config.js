912fe0d9e1cc4dffc4b819567d3a6bbdc7d9a9d6
IIIRN STUDIO <95156967+IIIRN@users.noreply.github.com>
2025-05-03 18:54:29 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+
+// กำหนด LIFF ID (EVENT)
+const LIFF_EVENT = 'XX'; // LIFF ID EVENT
+const LIFF_HISTORY = 'XX'; // LIFF ID HISTORY 
+
+// กำหนด API URL (Admin)
+const ADMIN_URL = "XX";
+const MEMBER_URL = 'XX';
+const ADDEVENT_URL = "XX";
+
+// กำหนด API URL (user)
+const EVENT_URL = 'XX';
