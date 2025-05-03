77c9c7abbc9fc86fdcc79793e233ca2d7b8df256
IIIRN STUDIO <95156967+IIIRN@users.noreply.github.com>
2025-05-03 19:18:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 
 // กำหนด LIFF ID (EVENT)
-const LIFF_EVENT = 'XX'; // LIFF ID EVENT
-const LIFF_HISTORY = 'XX'; // LIFF ID HISTORY 
+const LIFF_EVENT = '2006974351-8XYV44Ad'; // LIFF ID EVENT
+const LIFF_HISTORY = '2006974351-BDnJooQa'; // LIFF ID HISTORY 
 
 // กำหนด API URL (Admin)
-const ADMIN_URL = "XX";
-const MEMBER_URL = 'XX';
-const ADDEVENT_URL = "XX";
+const ADMIN_URL = "https://script.google.com/macros/s/AKfycbz_yeToniZMtx1-tZCF62YI9eWV6IGQnHYBQyrdnd2nOgJx2dhyDg3z-bsJmQ8kvls6IQ/exec";
+const MEMBER_URL = 'https://script.google.com/macros/s/AKfycbzF2GYdAm89Gjb38mKCQ2tKhdPpYFgkzsAYlnv03Ow34MEn5_ZbsQdpc7gSlRGIPwbiTg/exec';
+const ADDEVENT_URL = "https://script.google.com/macros/s/AKfycbx-pnhaEboYgy2OFWn4qVcjM3_RcVyoR9H6T-ZohS9Ndi3cI7SLa7-XVMlggN-ZOImS/exec";
 
 // กำหนด API URL (user)
-const EVENT_URL = 'XX';
+const EVENT_URL = 'https://script.google.com/macros/s/AKfycbyv0ZzJ2Pepr42-Z3NhQAOPmEP7-6hMtQAiITEyT86r_v7stpiNjeo1syCKtmk9yxf1zw/exec';
